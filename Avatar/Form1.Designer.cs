e7724a1bb0ea4a2bced4d0872eae7971b548e8fb
Mr Games 4 Life <150128847+MrGames4Life@users.noreply.github.com>
2023-12-15 14:41:13 +0100

    Add files via upload

---
+++ Avatar/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿
+namespace Avatar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(122, 273);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(151, 296);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Black;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Avatar/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Web;
+using System.Net;
+using System.Diagnostics;
+using System.IO;
+using System.Media;
+
+namespace Avatar
+{
+    public partial class Form1 : Form
+    {
+        private System.Windows.Forms.Timer timer;
+        private Random random;
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+
+            // Crear el temporizador
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 2000; // 2 segundos
+            timer.Tick += Timer_Tick;
+
+            // Crear el generador de números aleatorios
+            random = new Random();
+
+            // Iniciar el temporizador
+            timer.Start();
+            
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Cerrar la ventana
+            this.Hide();
+
+            // Generar coordenadas aleatorias dentro de los límites de la pantalla
+            int x = random.Next(Screen.PrimaryScreen.Bounds.Width - this.Width);
+            int y = random.Next(Screen.PrimaryScreen.Bounds.Height - this.Height);
+
+            // Establecer la nueva ubicación de la ventana
+            this.Location = new Point(x, y);
+
+            // Abrir la ventana nuevamente después de 2 segundos
+            timer.Stop(); // Detener el temporizador para evitar que se abra la ventana repetidamente
+            timer.Interval = 2000; // 2 segundos
+            timer.Start(); // Iniciar el temporizador nuevamente
+
+            this.Show(); // Abrir la ventana
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("¡Buenos días!", "Mr G4L");
+
+            // Mostrar el cuadro de diálogo de solicitud de permiso
+            DialogResult result = MessageBox.Show("¿Quieres escuchar música?", "Mr G4L", MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk);
+
+            // Si se concede el permiso, reproducir el archivo de audio
+            if (result == DialogResult.Yes)
+            {
+                string url = "https://drive.google.com/uc?export=download&id=1JEPUXC2tmPKKy1A-iWVWr0Qe5PN-KllC";
+                Process.Start(url);
+                MessageBox.Show("Descargué el archivo de música. ¡Ya está listo para ser escuchado!", "Mr G4L", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                string downloadsPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Downloads";
+                Process.Start(downloadsPath);
+                MessageBox.Show("¡Aquí está!", "Mr G4L", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+
+
+        }
+    }
+}
+
